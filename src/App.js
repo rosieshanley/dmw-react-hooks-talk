d577c2a3d5b309c324ca66557fa25906457309dc
rosieshanley <rosemary.shanley@slalom.com>
2019-04-09 22:07:09 -0600

    updating to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import 'react-rater/lib/react-rater.css';
 import {
@@ -10,116 +10,80 @@ import {
 import Review from './Review';
 import ReviewForm from './ReviewForm';
 import Reaction from './Reaction';
+import initialReviews from './initialReviews';
 import maui from './assets/maui-art.png';
 import title from './assets/title.png';
 
-class App extends Component {
-  state = {
-    reviews: [
-      {
-        text: "Stole us fire from down below! That's been really helpful.",
-        rating: 5,
-        datetime: 1551642810975,
-      },
-      {
-        text: "Brought us coconuts...but I'm allergic.",
-        rating: 3,
-        datetime: 1551642517315,
-      },
-      {
-        text:
-          'He tried to steal my boat and abandon me on an island alone. Not cool.',
-        rating: 1,
-        datetime: 1550641410975,
-      },
-      {
-        text: 'He has cool tattoos and a lot of confidence.',
-        rating: 4,
-        datetime: 1541641010475,
-      },
-      {
-        text: 'Kind of an egomaniac.',
-        rating: 2,
-        datetime: 1541641010475,
-      },
-    ],
-    currentIndex: 0,
-    currentPage: 1,
+function App() {
+  let [reviews, setReviews] = useState(initialReviews);
+  let [currentIndex, setCurrentIndex] = useState(0);
+  let [currentPage, setCurrentPage] = useState(1);
+
+  const pageSize = 4;
+  const paginatedReviews = getPage(reviews, currentPage, pageSize);
+
+  const handlePaginationClick = page => {
+    setCurrentPage(page);
   };
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        currentIndex: (this.state.currentIndex + 1) % this.state.reviews.length,
-      });
-    }, 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  handlePaginationClick = page => {
-    this.setState({ currentPage: page });
+  const addReview = (text, rating) => {
+    const newReviews = [{ text, rating, datetime: Date.now() }, ...reviews];
+    setReviews(newReviews);
   };
 
-  addReview = (text, rating) => {
-    const newReviews = [
-      { text, rating, datetime: Date.now() },
-      ...this.state.reviews,
-    ];
-    this.setState({ reviews: newReviews });
-  };
+  // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentIndex((currentIndex + 1) % reviews.length);
+    }, 3000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [currentIndex, reviews]);
 
-  render() {
-    const { reviews, currentIndex, currentPage } = this.state;
-    const pageSize = 4;
-    const paginatedReviews = getPage(reviews, currentPage, pageSize);
-
-    return (
-      <div className="App">
-        <div className="content content--left">
-          <div className="reaction">
-            <Reaction review={reviews[currentIndex]} />
-          </div>
+  return (
+    <div className="App">
+      <div className="content content--left">
+        <div className="reaction">
+          <Reaction review={reviews[currentIndex]} />
         </div>
-        <div className="content content--right">
-          <div className="review-board">
-            <div className="review-board__header">
-              <img className="reversed" src={maui} alt="Maui" />
-              <img
-                src={title}
-                className="review-board__header__title"
-                alt="Maui's Kudos Board"
+      </div>
+      <div className="content content--right">
+        <div className="review-board">
+          <div className="review-board__header">
+            <img className="reversed" src={maui} alt="Maui" />
+            <img
+              src={title}
+              className="review-board__header__title"
+              alt="Maui's Kudos Board"
+            />
+            <img src={maui} alt="Maui" />
+          </div>
+          <div className="review-form-container">
+            Grateful for all that Maui has done for mankind?
+            <br />
+            <strong>Leave a review!</strong>
+            <ReviewForm addReview={addReview} />
+          </div>
+          <div className="review-container">
+            {paginatedReviews.map((review, index) => (
+              <Review key={index} index={index} review={review} />
+            ))}
+            <div className="pagination-container">
+              <Pagination
+                current={currentPage}
+                total={reviews.length}
+                onChange={handlePaginationClick}
+                prevIcon={PaginationPrev}
+                nextIcon={PaginationNext}
+                pageSize={pageSize}
               />
-              <img src={maui} alt="Maui" />
-            </div>
-            <div className="review-form-container">
-              Grateful for all that Maui has done for mankind?
-              <br />
-              <strong>Leave a review!</strong>
-              <ReviewForm addReview={this.addReview} />
-            </div>
-            <div className="review-container">
-              {paginatedReviews.map((review, index) => (
-                <Review key={index} index={index} review={review} />
-              ))}
-              <div className="pagination-container">
-                <Pagination
-                  current={currentPage}
-                  total={reviews.length}
-                  onChange={this.handlePaginationClick}
-                  prevIcon={PaginationPrev}
-                  nextIcon={PaginationNext}
-                  pageSize={pageSize}
-                />
-              </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ src/initialReviews.js
@@ -0,0 +1,30 @@
+const initialReviews = [
+  {
+    text: "Stole us fire from down below! That's been really helpful.",
+    rating: 5,
+    datetime: 1551642810975,
+  },
+  {
+    text: "Brought us coconuts...but I'm allergic.",
+    rating: 3,
+    datetime: 1551642517315,
+  },
+  {
+    text:
+      'He tried to steal my boat and abandon me on an island alone. Not cool.',
+    rating: 1,
+    datetime: 1550641410975,
+  },
+  {
+    text: 'He has cool tattoos and a lot of confidence.',
+    rating: 4,
+    datetime: 1541641010475,
+  },
+  {
+    text: 'Kind of an egomaniac.',
+    rating: 2,
+    datetime: 1541641010475,
+  },
+];
+
+export default initialReviews;
